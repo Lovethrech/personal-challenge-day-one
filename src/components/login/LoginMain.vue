2ea1e6c1e8ede5c6b6a066115eed6b4a01004436
Lovethrech <rachealdolapo45@gmail.com>
2026-05-12 20:14:53 +0100

    ...working on Login page

--- src/components/login/LoginMain.vue
+++ src/components/login/LoginMain.vue
@@ -14,6 +14,7 @@ const PageTitleName=ref("TaskBoard");
                 Real-time Collaborative Workspace
             </p>
             <LoginForm/>
+            
         </div>
     </div>
 </template>
