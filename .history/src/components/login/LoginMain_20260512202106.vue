fa07ffb65e3d4eb1e528f9a184f1b098710f49ed
Lovethrech <rachealdolapo45@gmail.com>
2026-05-12 20:22:48 +0100

    ...working on Login page

---
+++ .history/src/components/login/LoginMain_20260512202106.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {ref} from "vue";
+import PageTitle from '../PageTitle.vue';
+import LoginForm from "./LoginForm.vue";
+
+const PageTitleName=ref("TaskBoard");
+</script>
+
+<template>
+    <div class="login-main">
+        <div class="login-card">
+            <PageTitle :name="PageTitleName"/>
+            <p class="subtitle">
+                Real-time Collaborative Workspace
+            </p>
+            <LoginForm/>
+            <div class="switch-auth">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.login-main{
+    width: 100%;
+    min-height: 100vh;
+    padding: 4vh 4vw;
+    display: flex;
+    place-items:center;
+    justify-content: center;
+}
+.login-card{
+    background-color: black;
+    padding: 2vh 4vh;
+}
+.subtitle{
+    text-align: center;
+    letter-spacing: 1vh;
+    font-size: 1.4vh;
+}
+</style>
