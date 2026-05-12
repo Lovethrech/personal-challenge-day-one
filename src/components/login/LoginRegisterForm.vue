1df0358e7a720b062b2fae82f209015fa1c75356
Lovethrech <rachealdolapo45@gmail.com>
2026-05-12 20:26:12 +0100

    ...working on Login page

---
+++ src/components/login/LoginRegisterForm.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {ref} from "vue";
+import { useRouter } from "vue-router";
+import authDetails from "../data/authDetails.json";
+import LoginFormLabelAndInput from './LoginFormLabelAndInput.vue';
+import LoginBtn from "./LoginBtn.vue";
+
+const router=useRouter();
+const disableBtn=ref(false);
+const emailValue=ref("");
+const passwordValue=ref("");
+
+const handleLogin=async(event)=>{
+    event.preventDefault();
+    
+    if ((emailValue.value.trim() !=="") && (passwordValue.value.trim() !== "")){
+        disableBtn.value=true;
+        console.log('Login attempt', emailValue.value);
+        router.push('/dashboard');
+    }
+    else{
+        console.warn("Email and/or password is empty");
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="handleLogin" class="login-form">
+        <LoginFormLabelAndInput 
+            :vModel="emailValue" 
+            :name="authDetails[0].name" 
+            :nameType="authDetails[0].nameType" 
+            :placeholder="authDetails[0].placeholder"
+        />
+        <LoginFormLabelAndInput 
+            :vModel="passwordValue" 
+            :name="authDetails[1].name" 
+            :nameType="authDetails[1].nameType" 
+            :placeholder="authDetails[1].placeholder"
+        />
+        <br/>
+        <LoginBtn @click="handleLogin" :disableBtn="disableBtn"/>
+    </form>
+</template>
+
+<style scoped>
+.login-form{
+    margin-top: 10vh;
+}
+</style>
